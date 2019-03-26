bc35bcf1832336433e996e7f00d89c4686c5be99
imil4eg <emzilh@mail.ru>
2019-03-26 23:13:07 +0300

    Removed foreign key to DiaseOutcomeType entity from Appointment entity and Added foreigrn key to Appointment to DiseaseOutcomeType entity

--- MedicalExamination.Entities/DiseaseOutcomeType.cs
+++ MedicalExamination.Entities/DiseaseOutcomeType.cs
@@ -1,9 +1,18 @@
-﻿namespace MedicalExamination.Entities
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MedicalExamination.Entities
 {
     /// <summary>
     /// Dictionary of disease outcomes
     /// </summary>
-    public sealed class DiseaseOutcomeType : BaseTypeEntity
-    { 
+    public sealed class DiseaseOutcomeType
+    {
+        public Guid AppointmentId { get; set; }
+        [ForeignKey("AppointmentId")]
+        public Appointment Appointment { get; set; }
+
+        public string Name { get; set; }
     }
 }
